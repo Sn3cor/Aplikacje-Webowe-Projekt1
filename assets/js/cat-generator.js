fd4af801cf415a8a385c406421c5fac245ffa2a7
Sn3cor <jakub.krupa23@gmail.com>
2025-11-06 01:46:59 +0100

    cleanup and actual final touch

--- assets/js/cat-generator.js
+++ assets/js/cat-generator.js
@@ -14,7 +14,6 @@ const generateNextCat = (cat) => {
 }
 
 document.querySelector('.cat').addEventListener('click', (e) => {
-    console.log(e);
     const cat = e.target;
     generateNextCat(cat);
 })
--- assets/js/form-validation.js
+++ assets/js/form-validation.js
@@ -1,5 +1,4 @@
-document.getElementById("clear").addEventListener("click", (e) => {
-    console.log(e);
+document.getElementById("clear").addEventListener("click", () => {
     document.getElementById("form").reset();
 })
 
@@ -24,14 +23,14 @@ form.toggleAttribute('novalidate', true);
 form.addEventListener("input", e => {
 
     const input = e.target;
-    console.log(input)
+    // console.log(input)
     input.parentElement.querySelector(".form-error").innerHTML = '';
     input.style.borderColor = 'var(--font-color)'
 })
 
 form.addEventListener("submit", e => {
     const inputs = Array.from(e.currentTarget.querySelectorAll("input, textarea, select"));
-    console.log(inputs);
+    // console.log(inputs);
     for (const input of inputs) {
         inp_validity = customValidity(input);
         if (inp_validity) {
